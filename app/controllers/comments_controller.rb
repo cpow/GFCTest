1cfc44a28026447dcab32481fb73ac92d44e2319
Chris Power <cpow85@gmail.com>
2009-10-01 11:40:15 -0400

    removed some JS

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -53,10 +53,7 @@ class CommentsController < ApplicationController
       
        @post = Post.find(params[:post_id])
        @comment = @post.comments.create!(params[:comment])
-
-       respond_to do |format|
-          format.html {redirect_to @post}
-          format.js
+       redirect_to @post
    end
  end
 
