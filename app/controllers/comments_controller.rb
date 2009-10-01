372ff4bd185d7cbc8035dd4f47599f34ffb2664e
Chris Power <cpow85@gmail.com>
2009-10-01 11:47:05 -0400

    small change

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -50,11 +50,9 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.xml
   def create
-      
        @post = Post.find(params[:post_id])
        @comment = @post.comments.create!(params[:comment])
        redirect_to @post
-   end
  end
 
   # PUT /comments/1
